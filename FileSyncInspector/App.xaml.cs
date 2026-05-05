13eb93f0ffe5706ae98360a73e3e9af66a48b5a0
Robert Strakoš <vip.strakos@gmail.com>
2026-05-06 01:03:21 +0200

    Redesign UI to Fluent Design and add EN/CS localization

--- FileSyncInspector/App.xaml.cs
+++ FileSyncInspector/App.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
-
+using System.Globalization;
 namespace FileSyncInspector
 {
     /// <summary>
@@ -9,6 +9,14 @@ namespace FileSyncInspector
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Vynucení angličtiny (pokud to zakomentuješ, vezme se jazyk Windows)
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+
+            base.OnStartup(e);
+        }
     }
 
 }
